0edf7c22065ecf1449451639701cd53d3e1c9358
Lucas Lee <me@lucaslee.io>
2022-11-24 14:52:09 +0800

    add autocomplete

--- aws-mfa.plugin.zsh
+++ aws-mfa.plugin.zsh
@@ -1,3 +1,4 @@
 source ~/.oh-my-zsh/custom/plugins/aws-mfa/getaws
+source ~/.oh-my-zsh/custom/plugins/aws-mfa/getaws_complete
 source ~/.oh-my-zsh/custom/plugins/aws-mfa/clearaws
 alias aws-mfa='getaws'
